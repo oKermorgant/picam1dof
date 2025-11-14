c2262962cb5787e172630c60476ea79db4250cbb
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2025-11-14 17:09:31 +0100

    use v4l2 for cam name

--- launch/picam_launch.py
+++ launch/picam_launch.py
@@ -5,12 +5,15 @@ def generate_launch_description():
 
     sl = SimpleLauncher()
 
+    dev = sl.declare_arg('device', default_value=0, description='video device')
     sl.declare_arg('width', default_value=800, description='image width')
     sl.declare_arg('height', default_value=600, description='image height')
 
-    sl.node('camera_ros', 'camera_node',
+    sl.node('v4l2_camera', 'v4l2_camera_node',
             parameters = [sl.arg_map('width', 'height'),
-                        {'camera_info_url': 'file://'+sl.find('picam1dof', 'picam.yaml')}],
+                        {'output_encoding': 'rgb8',
+                        'camera_info_url': 'file://'+sl.find('picam1dof', 'picam.yaml'),
+                        'video_device': '/dev/video' + dev}],
             remappings={'image_raw': 'image', 'image_raw/compressed': 'image/compressed'})
 
 
