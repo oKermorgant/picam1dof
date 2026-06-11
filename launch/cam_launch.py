0737873ff566753c714b25ce33eb8afaf9f3acb0
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2026-06-11 12:19:34 +0200

    separate cam launch

---
+++ launch/cam_launch.py
@@ -0,0 +1,42 @@
+from simple_launch import SimpleLauncher
+from ament_index_python.packages import get_package_prefix
+
+def generate_launch_description():
+
+    sl = SimpleLauncher()
+
+    has_camera_ros = False
+
+    try:
+        get_package_prefix('camera_ros')
+        has_camera_ros = True
+    except:
+        pass
+
+
+    cam_info = 'file://' + sl.find('picam1dof', 'picam.yaml')
+    dev = '/dev/video0'
+    frame_id = 'camera'
+    height = 600
+    width = 800
+
+    if has_camera_ros:
+        sl.node('camera_ros', 'camera_node', name = 'camera',
+                remappings = {'~/camera_info': 'camera_info',
+                              '~/image_raw/compressed': 'image/compressed'},
+                parameters = {'camera': 1,
+                              'camera_info_url': cam_info,
+                              'frame_id': frame_id,
+                              'format': 'MJPEG',
+                              'width': width,
+                              'height': height})
+    else:
+        sl.node('v4l2_camera', 'v4l2_camera_node', name = 'camera',
+        parameters = {#'pixel_format': 'mjpg',
+                        'video_device': dev,
+                        'camera_frame_id': frame_id,
+                        'image_size': [width,height],
+                        'camera_info_url': cam_info})
+
+
+    return sl.launch_description()
--- launch/picam_launch.py
+++ launch/picam_launch.py
@@ -5,16 +5,18 @@ def generate_launch_description():
 
     sl = SimpleLauncher()
 
-    dev = sl.declare_arg('device', default_value=0, description='video device')
-    sl.declare_arg('width', default_value=800, description='image width')
-    sl.declare_arg('height', default_value=600, description='image height')
-
-    sl.node('v4l2_camera', 'v4l2_camera_node',
-            parameters = [sl.arg_map('width', 'height'),
-                        {'output_encoding': 'rgb8',
-                        'camera_info_url': 'file://'+sl.find('picam1dof', 'picam.yaml'),
-                        'video_device': '/dev/video' + dev}],
-            remappings={'image_raw': 'image', 'image_raw/compressed': 'image/compressed'})
+    sl.include('picam1dof', 'cam_launch.py')
+    #
+    # dev = sl.declare_arg('device', default_value=0, description='video device')
+    # sl.declare_arg('width', default_value=800, description='image width')
+    # sl.declare_arg('height', default_value=600, description='image height')
+    #
+    # sl.node('v4l2_camera', 'v4l2_camera_node',
+    #         parameters = [sl.arg_map('width', 'height'),
+    #                     {'output_encoding': 'rgb8',
+    #                     'camera_info_url': 'file://'+sl.find('picam1dof', 'picam.yaml'),
+    #                     'video_device': '/dev/video' + dev}],
+    #         remappings={'image_raw': 'image', 'image_raw/compressed': 'image/compressed'})
 
 
     # also, run the PWM
